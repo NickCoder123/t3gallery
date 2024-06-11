747270d39c25276a8495d52e606fff375a297ae9
Nikunj Singh <nikunjmehra935@gmail.com>
2024-06-11 19:13:15 +0530

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+const mockUrls = [
+  "https://utfs.io/f/aaeadd19-ce7a-469c-8d6b-1365f94e9b4d-2dkq.png",
+  "https://utfs.io/f/0d65e418-3aeb-4e55-b771-bf783c8d1978-tpk6wk.com_wallpaper.jpg",
+  "https://utfs.io/f/48601eca-114f-434c-8d76-85748f7f2ee1-1txbs.jpg",
+  "https://utfs.io/f/7e03e478-e527-417c-aa99-94a2419d66a1-1d.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello! (gallery under construction)
+    <main>
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
